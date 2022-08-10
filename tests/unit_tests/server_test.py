148594ae6b0301748f3abefa8109bb5c07010c26
Thomas Roussel <thomas.roussel@ik.me>
2022-08-10 18:53:14 +0200

    valid, invalid, passed competition tests

--- tests/unit_tests/server_test.py
+++ tests/unit_tests/server_test.py
@@ -58,6 +58,44 @@ class TestShowSummary(TestServer):
         assert b'<li>This email is not registered</li>' in rv.data
 
 
+class TestBook(TestServer):
+
+    def test_should_return_book_page_if_club_and_competition_are_valid(self, client, monkeypatch,
+                                                                       example_club_instances,
+                                                                       example_competition_instances):
+        monkeypatch.setattr(server, 'clubs', example_club_instances)
+        monkeypatch.setattr(server, 'competitions', example_competition_instances)
+
+        rv = client.get('/book/Test competition 1/Test club 1')
+
+        assert rv.status_code == 200
+        assert b'<li>You can now book places for this competition</li>' in rv.data
+        assert b'<h2>Test competition 1</h2>' in rv.data
+        assert b'<input type="hidden" name="club" value="Test club 1">' in rv.data
+
+    def test_should_return_404_if_club_or_competition_is_invalid(self, client, monkeypatch,
+                                                                 example_club_instances,
+                                                                 example_competition_instances):
+        monkeypatch.setattr(server, 'clubs', example_club_instances)
+        monkeypatch.setattr(server, 'competitions', example_competition_instances)
+
+        rv = client.get('/book/Wrong competition/Wrong club')
+
+        assert rv.status_code == 404
+
+    def test_should_return_book_page_if_competition_date_is_passed(self, client, monkeypatch,
+                                                                   example_club_instances,
+                                                                   example_competition_instances):
+        monkeypatch.setattr(server, 'clubs', example_club_instances)
+        monkeypatch.setattr(server, 'competitions', example_competition_instances)
+
+        rv = client.get('book/Test competition 4/Test club 1')
+
+        assert rv.status_code == 200
+        assert b'<li>This competition is no longer available</li>' \
+               in rv.data
+
+
 class TestPurchasePlaces(TestServer):
 
     def test_should_return_show_summary_page_if_booking_conditions_are_valid(self, client, monkeypatch,
